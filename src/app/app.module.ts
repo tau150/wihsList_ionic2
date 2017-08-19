a0b220cee985cfb1554365c356ffccccbff32955
Santi Nuñez <tau150@hotmail.com>
2017-08-19 17:09:34 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,17 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
+    PendientesComponent,
+    TerminadosComponent,
     TabsPage
   ],
   imports: [
@@ -29,6 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    PendientesComponent,
+    TerminadosComponent,
     TabsPage
   ],
   providers: [
